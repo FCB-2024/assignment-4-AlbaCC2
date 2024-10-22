89b5a19c4605cc6859572687648687b7fff1b201
Alba Comino <alba.comino01@estudiant.upf.edu>
2024-10-22 10:24:34 +0200

    first try

--- src/antiprime.py
+++ src/antiprime.py
@@ -1,24 +1,33 @@
 ## ADD WHATEVER ARGUMENTS ARE NECESSARY TO THE MAIN FUNCTION
 ## IN THE SAME ORDER AS THE ARGUMENTS ARE TAKEN FROM THE
 ## COMMAND LINE SPECIFIED BELOW
-def main() :
-	## YOU CODE SHOULD START HERE AST THE SAME
-	## IDENTATION AS THIS COMMENT
+import sys
+def main(x) :
+	r = 0
+	i = 1
+	while i <= x :
+		if x % i == 0 :
+			r = r + 1
+		i = i + 1
+	return r
 
-
-	## THE LAST LINES OF YOUR CODE SHOULD EITHER
-	## RETURN THE VALUE "anti-prime" or "not anti-prime"
-	## REPLACE THE FOLLOWING LINE BY WHATEVER LINES
-	## OF CODE ALLOW THIS FUNCTION TO RETURN THE VALUE
-	## "anti-prime" or "not anti-prime"
+def ap(x):
+	divisores = main(x)
+	i = 1
+	while i < x :
+		if main(i) >= divisores :
+			return "not anti-prime"
+		i = i + 1
 	return("anti-prime")
 
 ## DO NOT REMOVE THIS LINE BELOW
 if __name__ == "__main__" :
 
-	## MODIFY THE LINE BELOW AND ADD BEFORE WHATEVER LINES ARE NECESSARY
-	## TO RUN THIS PROGRAM AS, FOR INSTANCE:
+	num = int(sys.argv[1])
+	res = ap(num)
+	print(res)
+	
+
 	## $ python antiprime.py 6
 	## WHERE THE FIRST ARGUMENT IS A POSITIVE INTEGER NUMBER FOR WHICH
 	## YOU WANT TO FIGURE OUT WHETHER IS ANTI-PRIME OR NOT
-	print(main())
